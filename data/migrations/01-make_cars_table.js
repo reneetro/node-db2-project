7bdbcd07fff185d524c5fffd2dc0d841b18cfbbc
renee.troiani@gmail.com <renee.troiani@gmail.com>
2022-06-14 12:09:02 -0600

    make cars migration complete

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,17 @@
 exports.up = function (knex) {
   // DO YOUR MAGIC
+  return knex.schema.createTable('cars', tbl => {
+    tbl.increments('id');
+    tbl.varchar('vin').unique().notNullable();
+    tbl.varchar('make', 128).notNullable();
+    tbl.varchar('model', 128).notNullable();
+    tbl.numeric('milage').notNullable();
+    tbl.varchar('title', 128);
+    tbl.varchar('transmission');
+  })
 };
 
 exports.down = function (knex) {
   // DO YOUR MAGIC
+  return knex.dropTableIfExists('cars')
 };
